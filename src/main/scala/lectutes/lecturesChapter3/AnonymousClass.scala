0879bdd45f334547ca7b55d85cdb71a104d72669
Chuan <kirk336neo@gmail.com>
2024-03-23 21:54:13 +0800

    Finish Chapter 3 Lectures 19

---
+++ src/main/scala/lectutes/lecturesChapter3/AnonymousClass.scala
@@ -0,0 +1,14 @@
+package lectutes.lecturesChapter3
+
+object AnonymousClass extends App {
+  abstract class Animal {
+    def eat: Unit
+  }
+  
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("ahahahahahahaha")
+    
+  }
+  println(funnyAnimal.getClass)
+}
