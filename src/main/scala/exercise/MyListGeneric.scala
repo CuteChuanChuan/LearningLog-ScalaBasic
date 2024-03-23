365c856de9e8640151e94802a66e5f786463f6f6
Chuan <kirk336neo@gmail.com>
2024-03-23 19:36:07 +0800

    Finish Chapter 3 Lectures 18

---
+++ src/main/scala/exercise/MyListGeneric.scala
@@ -0,0 +1,36 @@
+package exercise
+
+abstract class MyListGeneric[+A] {
+  def head: A
+  def tail: MyListGeneric[A]
+  def isEmpty: Boolean
+  def add[B >: A](element: B): MyListGeneric[B]
+  def printElements: String
+  override def toString: String = "[" + printElements + "]"
+}
+
+object EmptyListGeneric extends MyListGeneric[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: Nothing = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add[B >: Nothing](element: B): MyListGeneric[B] = new ConsGeneric(element, EmptyListGeneric)
+  def printElements: String = ""
+}
+
+class ConsGeneric[+A](h: A, t: MyListGeneric[A]) extends MyListGeneric[A] {
+  def head: A = h
+  def tail: MyListGeneric[A] = t
+  def isEmpty: Boolean = false
+  def add[B >: A](element: B): MyListGeneric[B] = new ConsGeneric(element, this)
+  def printElements: String = {
+    if(t.isEmpty) "" + h
+    else s"$h ${t.printElements}"
+  }
+}
+
+object GenericListTest extends App {
+  val listInt: MyListGeneric[Int] = new ConsGeneric[Int](1, new ConsGeneric[Int](2, new ConsGeneric[Int](3, EmptyListGeneric)))
+  println(listInt.toString)
+  val listStr: MyListGeneric[String] = new ConsGeneric[String]("first", new ConsGeneric[String]("second", new ConsGeneric[String]("three", EmptyListGeneric)))
+  println(listStr.toString)
+}
---
+++ src/main/scala/lectutes/lecturesChapter3/Generics.scala
@@ -0,0 +1,42 @@
+package lectutes.lecturesChapter3
+
+object Generics extends App {
+  class MyList[+SpecificType] {
+    def add[B >: SpecificType](element: B): MyList[B] = ???
+  }
+  
+  val listOfInt = new MyList[Int]
+  val listOfString = new MyList[String]
+  
+  object MyList {
+    def empty[A]: MyList[A] = ???
+  }
+  val emptyListOfIntegers = MyList.empty[Int]
+  
+  // variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+  
+  // 1. covariance
+  class CovarianceList[+A]
+  val animal: Animal = new Cat
+  val animalList: CovarianceList[Animal] = new CovarianceList[Cat]
+  // animalList.add(new Dog) ?? -> a list of animals
+  
+  
+  // 2. invariance
+  class InvariantList[A]
+  val invariantList: InvariantList[Animal] = new InvariantList[Animal]
+  
+  // 3. contravatiance
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+  
+  //bounded tpyes
+  class Cage[A <: Animal](animal: A)
+  val cage = new Cage(new Dog)
+  
+  class Car
+//  val newCage = new Cage(new Car)
+}
