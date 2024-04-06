5db414d21098f999a98ae53affa4159061dda293
Chuan <kirk336neo@gmail.com>
2024-04-06 15:06:28 +0800

    Finish Chapter 3 Lectures 23

---
+++ src/main/scala/lectutes/lecturesChapter3/ExceptionExercise.scala
@@ -0,0 +1,10 @@
+package lectutes.lecturesChapter3
+
+object ExceptionExercise extends App {
+  object PocketCalculator {
+    def add(x: Int, y: Int) = {
+      val result = x + y
+      result
+    }
+  }
+}
--- src/main/scala/lectutes/lecturesChapter3/Exceptions.scala
+++ src/main/scala/lectutes/lecturesChapter3/Exceptions.scala
@@ -1,5 +1,19 @@
 package lectutes.lecturesChapter3
 
-object Exceptions {
-
+object Exceptions extends App {
+  def getInt(withExceptions: Boolean): Int =
+    if (withExceptions) throw new RuntimeException("No int for you")
+    else 42
+  
+  try {
+    getInt(withExceptions = true)
+  } catch {
+    case e: RuntimeException => println("Runtime exception")
+  } finally {
+    println("Finally")
+  }
+  
+  private class MyException extends Exception
+  private val exception = new MyException
+  throw  exception
 }
